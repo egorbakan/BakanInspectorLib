986940d0787a8fbcf7f783ff7121b565cc194b99
egorbakan <118420911+egorbakan@users.noreply.github.com>
2022-11-29 15:31:12 +0900

    Add files via upload

---
+++ ИтоговоеЗадание3/InspectorLib/FunctionInsp.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace InspectorLib
+{
+    public class FunctionInsp
+    {
+        string[] AllEmployee = { "Иванов И.И.", "Зиронов Т.А.", "Миронов А.В.", "Васильев В.И." };//Список всех сотрудников Автоинспекции
+        string[] MainEmployee = { "Васильев Василий Иванович" };//ФИО главного инспектора
+
+        public void GetInspector(string name)//Метод возвращает ФИО главного инспектора
+        {
+            Console.WriteLine($"Главный инспектор – {name}");//Выводит именя глав. инспектора
+        }
+
+        public void GetСarInspection(string TitleInspection)//Метод возвращает название автоинспекции
+        {
+            Console.WriteLine($"Название – {TitleInspection}");//Вывод названия автоинспекции
+        }
+
+        public string SetInspector(string MainIspector)//С помощью этого метода можно изменять ФИО главного автоинспектора
+        {
+            Console.WriteLine("Выберите имя инспектора (Напишите число от 1 - 4)");
+            int a = Convert.ToInt32(Console.ReadLine());
+            if (a == 1)
+            {
+                MainIspector = AllEmployee[0];
+                AllEmployee = MainEmployee;
+                Console.WriteLine($"Глав. инспектор - {MainIspector}");
+                return MainIspector;
+            }
+            else if (a == 2)
+            {
+                MainIspector = AllEmployee[1];
+                AllEmployee = MainEmployee;
+                Console.WriteLine($"Глав. инспектор - {MainIspector}");
+                return MainIspector;
+            }
+            else if (a == 3)
+            {
+                MainIspector = AllEmployee[2];
+                AllEmployee = MainEmployee;
+                Console.WriteLine($"Глав. инспектор - {MainIspector}");
+                return MainIspector;
+            }
+            else if (a == 4)
+            {
+                MainIspector = AllEmployee[3];
+                AllEmployee = MainEmployee;
+                Console.WriteLine($"Глав. инспектор - {MainIspector}");
+                return MainIspector;
+            }
+            else
+            {
+                Console.WriteLine("Напишите число от 1 - 4");//Вывод ошибки в консоль 
+                return MainIspector;
+            }
+        }
+
+        public string GenerateNumber(int govnumber, char symbols, int codeRegion = 75) //Метод возвращает сгенерированный госномер
+        {
+            string result = symbols + "";
+            result = result.ToUpper() + "-" + govnumber + "-" + codeRegion;
+            Console.WriteLine("Госномер:" + result);//Вывод госномера
+            return result;//Возращение переменной result
+        }
+
+        public void GetWorker()//Метод возращает список всех сотрудников
+        {
+            string[] FullName = new string[] { "Иванов И.И.", "Зиронов Т.А.", "Миронов А.В.", "Васильев В.И." };//Объявляем массив
+            Span<string> AllEmployeeSpan = AllEmployee;//Преобразование массива в Span
+
+            foreach (var Employees in AllEmployeeSpan)
+            {
+                Console.WriteLine("Список всех сотрудников автоинспекции: " + Employees); //вывод всех сотрудников
+            }
+        }
+
+        public void AddWorker()//добавление нового сотрудника
+        {
+            Console.WriteLine("Введите ФИО нового сотрудника");
+            string Employee = Console.ReadLine();//Ввод ФИО нового сотрудника
+            Array.Resize(ref AllEmployee, AllEmployee.Length + 1);
+            AllEmployee[AllEmployee.Length - 1] = Employee;//Смена размера массива
+            Console.WriteLine($"Сотрудник {Employee} добавлен");
+        }
+    }
+}
---
+++ ИтоговоеЗадание3/ИтоговоеЗадание3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using InspectorLib;
+
+namespace ИтоговоеЗадание3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FunctionInsp func = new FunctionInsp();
+
+            func.GetInspector("Васильев Василий Иванович");//Вывод главного инспектора
+            func.GetСarInspection("Автоинспекция г. Чита");//Вывод названия автоинспекции
+            func.SetInspector("");//Изменение ФИО главного инспектора
+            func.GenerateNumber(123, 'Л');//Вывод госномера
+            func.GetWorker();//Вывод списка сотрудников автоинспекции
+            func.AddWorker();//Добавление сотрудника автоинспекции
+        }
+    }
+}
